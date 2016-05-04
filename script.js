54bd84699cb117b4727e245245736277e25d35b7
rocketNeck <homestarflyer@gmail.com>
2016-05-03 21:28:13 -0400

    Done.

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
   // Now we start the Ajax Get request. The first parameter is the URL with the data.
   // The second parameter is a function that handles the response.
   $.get("html/sentence.html", function(response) {
-    // Here we are getting the element on the page with the id of sentences and
-    // insert the response
+
     $("#sentences").html(response);
   });
 });
